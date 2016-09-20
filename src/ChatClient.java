aed406e3ce5297453fd8c34e50f9445779fe930d
Parmar, Amarjot <amarjot-parmar@hotmail.co.nz>
2016-09-21 00:05:03 +1200

    Created ChatClient Class

---
+++ src/ChatClient.java
@@ -0,0 +1,16 @@
+import java.io.IOException;
+import java.net.MulticastSocket;
+
+class ChatClient
+{
+
+
+    public static void main(String[] args) throws IOException {
+        MulticastSocket s = new MulticastSocket(40202);
+
+    }
+
+
+
+
+}

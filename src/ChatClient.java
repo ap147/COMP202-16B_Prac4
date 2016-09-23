09e1454e924ca4de95f448999a6a5e5b9e3a5cbe
Parmar, Amarjot <amarjot-parmar@hotmail.co.nz>
2016-09-24 00:32:30 +1200

    Documented, Thread spawned which listens to messages on MulticastSocket Project COMPLETED

--- src/ChatClient.java
+++ src/ChatClient.java
@@ -4,11 +4,13 @@ import java.io.InputStreamReader;
 import java.net.DatagramPacket;
 import java.net.InetAddress;
 import java.net.MulticastSocket;
-
+//https://www.mkyong.com/java/how-do-convert-byte-array-to-string-in-java/
 class ChatClient
 {
 
     public static void main(String[] args) throws IOException {
+        System.out.println("-------- CHAT CLIENT STARTED --------");
+        System.out.println();
         //Multicast that we wil be listening to
         InetAddress group = InetAddress.getByName("239.0.202.1");
         //Listening on port
@@ -21,35 +23,20 @@ class ChatClient
         DatagramPacket message;
         DatagramPacket recvmsg;
         byte [] buf;
-        String msg; String responseIP; String reponsemsg;
+        String msg; String responseIP;
         InetAddress sendersAdd;
-        boolean live =true;
-        while(live)
+
+        MulticastSocketThread Thread = new MulticastSocketThread(s);
+
+        Thread.start();
+        while(true)
         {
             //Getting what user typed in console & Sending it
             userInput = new BufferedReader(new InputStreamReader(System.in));
             msg = userInput.readLine();
             message = new DatagramPacket(msg.getBytes(), msg.length(), group, 40202);
             s.send(message);
-
-            //Getting what other useres responses
-            buf = new byte[1000];
-            recvmsg = new DatagramPacket(buf, buf.length);
-            s.receive(recvmsg);
-
-            //Printing response to console with there IP
-            sendersAdd = recvmsg.getAddress();
-            responseIP = sendersAdd.getHostAddress();
-            reponsemsg = new String(recvmsg.getData());
-            System.out.println(responseIP + ": " + reponsemsg);
-
-            if(reponsemsg.equals("STOP"))
-                live = false;
         }
-
     }
-
-
-
 
 }
---
+++ src/MulticastSocketThread.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+import java.net.SocketException;
+import java.nio.channels.DatagramChannel;
+
+//https://www.mkyong.com/java/how-do-convert-byte-array-to-string-in-java/
+class MulticastSocketThread extends Thread
+{
+    //Multicast Socket that we are listening in
+    MulticastSocket multicastSocket;
+
+    public MulticastSocketThread(MulticastSocket s) {
+        multicastSocket = s;
+    }
+
+    public void run()
+    {
+        //Store the packet thats being sent
+        DatagramPacket recvmsg;
+        byte [] buf;
+        //IP addess of whoever is sending a message
+        String responseIP;
+        InetAddress sendersAdd;
+        //Listening to Multicast socket passed in and printing out messages
+     while(true)
+     {
+         buf = new byte[1000];
+         //Message will be stored in this
+        recvmsg = new DatagramPacket(buf, buf.length);
+        try {
+            multicastSocket.receive(recvmsg);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Thread Error");
+        }
+
+        //Printing message to console with there IP
+        sendersAdd = recvmsg.getAddress();
+        responseIP = sendersAdd.getHostAddress();
+        System.out.println(responseIP + ": " + new String(recvmsg.getData()));
+      }
+    }
+}
